beb0c4b207c3915102db78a30c6fed5a23f13f99
AMADOBASS <caicaguarec@gmail.com>
2022-10-07 05:41:56 -0400

    Inicio del Proyecto, solo el menu principal sin ningun otro modulo

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+@extends('adminlte::page')
+
+@section('title', 'Escritorio')
+
+@section('content_header')
+    <h1>Escritorio</h1>
+@stop
+
+@section('content')
+    <p>Bienvenidos al sistema de recaudacion de fondos del deporte del Estado Anzoategui.</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
