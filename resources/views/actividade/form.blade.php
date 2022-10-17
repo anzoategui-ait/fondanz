0bda44a5a4f3bd0fe49fc0baab6f4302d120dfe7
AMADOBASS <caicaguarec@gmail.com>
2022-10-17 13:52:03 -0400

    Se agrego el formulario form de la actividad, que no se habia agregado antes

--- resources/views/actividade/form.blade.php
+++ resources/views/actividade/form.blade.php
@@ -13,8 +13,8 @@
 
         <div class="col-md-6">
         <div class="form-group">
-            {{ Form::label('subsector_id') }}
-            {{ Form::select('subsector_id',$subsectores, $actividade->subsector_id, ['class' => 'form-control' . ($errors->has('subsector_id') ? ' is-invalid' : ''), 'placeholder' => 'Subsector Id']) }}
+            {{ Form::label('subsector') }}
+            {{ Form::select('subsector_id',$subsectores, $actividade->subsector_id, ['class' => 'form-control' . ($errors->has('subsector_id') ? ' is-invalid' : ''), 'placeholder' => 'Seleccione un Subsector']) }}
             {!! $errors->first('subsector_id', '<div class="invalid-feedback">:message</div>') !!}
         </div>
         </div>
